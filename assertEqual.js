6aeb2bf0cc62068c42d351aa0c6f419d8897a730
DragonMess <kmylop@hotmail.com>
2020-01-06 22:41:59 +0000

    asserEqual tested:

---
+++ assertEqual.js
@@ -0,0 +1,23 @@
+// // // FUNCTION IMPLEMENTATION
+// // const sum = function(a, b) { 
+// //   return a + b;
+// // }
+// // //console.assert(sum(1, 2) === 3);
+// // console.assert(sum(1, 20) === 3);
+
+// // FUNCTION IMPLEMENTATION
+// const sumBuggy = function(a, b) { 
+//   return a * b;
+// }
+
+// // TEST CODE
+// console.assert(sumBuggy(1, 2) === 3); // fails, because bug!
+
+const asserEqual = function(actual,expected){
+
+};
+
+// TEST CODE
+
+asserEqual("Lighthouse Labs","Bootcamp");
+asserEqual(1,1);
